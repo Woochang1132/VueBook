b1315ce35c1d13353a62466d6cf84d04140e8163
Choi Woochang <show4031132@gmail.com>
2024-02-02 14:30:59 +0900

    VueBook : v-for 순환을 적용해보기

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <TransitionTag/>
   <MethodReturn/>
   <LinkValue/>
+  <VForDirective/>
 </template>
 
 <script>
 import TransitionTag from './components/TransitionTag.vue'
 import MethodReturn from './components/MethodReturn.vue'
 import LinkValue from './components/LinkValue.vue'
+import VForDirective from './components/VForDirective.vue'
 
 export default {
   name: 'App',
   components: {
     TransitionTag,
     MethodReturn,
-    LinkValue
+    LinkValue,
+    VForDirective
   }
 }
 </script>
---
+++ src/components/VForDirective.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li v-for="person in people" :key="person.id">
+            <div>{{ person.firstName }} {{ person.lastName }} - {{ person.dob }}</div>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                people: [
+                    {
+                        id: 1,
+                        firstName: "John",
+                        lastName : "Doe",
+                        dob : "01-01-1980"
+                    },
+
+                    {
+                        id: 2,
+                        firstName: "Johny",
+                        lastName : "Doe",
+                        dob : "01-01-1999"
+                    },
+
+                    {
+                        id: 3,
+                        firstName: "kate",
+                        lastName : "kim",
+                        dob : "01-01-2000"
+                    },
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
