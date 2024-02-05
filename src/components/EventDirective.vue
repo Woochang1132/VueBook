1cd51a5f3a6ded8a9e2b34f9b7f1fae34071d665
Choi Woochang <show4031132@gmail.com>
2024-02-05 10:39:13 +0900

    VueBook : vue에서의 이벤트 처리 방법

---
+++ src/components/EventDirective.vue
@@ -0,0 +1,24 @@
+<template>
+    <button type="button">
+        
+    </button>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+               
+            }
+        },
+        methods:{
+            handleClick: function (){
+                console.log('hello world')
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
