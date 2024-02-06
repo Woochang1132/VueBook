d6868a17ac0de3a306174c0c20ff28d358eb08ea
Choi Woochang <show4031132@gmail.com>
2024-02-06 18:16:38 +0900

    VueBook : props 정보전달을 통한 component  구성하기

---
+++ src/components/CheckboxItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <li>
+        <input type="checkbox" :checked="checked" />{{ name }}
+    </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "CheckboxItem",
+        props : ["name" , "checked"],
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
